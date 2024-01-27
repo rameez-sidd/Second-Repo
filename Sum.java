b2f6a4b319d9ea63c7e4e6b57f3f5a9ddc57f771
rameez-sidd <157888573+rameez-sidd@users.noreply.github.com>
2024-01-27 19:20:10 +0530

    Created Sum.java

---
+++ Sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sum{
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    int n1, n2;
+    System.out.print("Enter Number 1 : ");
+    n1 = s.nextInt();
+    System.out.print("Enter Number 2 : ");
+    n2 = s.nextInt();
+    System.out.println("The sum is : " + n1+n2);
+    
+  }
+}
